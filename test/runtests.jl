00326dbe02105d6d3abdff3c6455388e59f19878
Dilum Aluthge <dilum@aluthge.com>
2024-01-20 02:18:14 -0500

    Add more tests and set up CI (#2)

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,18 +15,15 @@ using Test: @test
             Meta.parse("@foo(bar)"),
         ]
         for expr in good_exprs
-            @testset let context = (; expr)
-                @test Public._is_valid_macro_expr(expr)
-            end
-
+            @test Public._is_valid_macro_expr(expr)
         end
         for expr in bad_exprs
-            @testset let context = (; expr)
-                @test !Public._is_valid_macro_expr(expr)
-            end
+            @test !Public._is_valid_macro_expr(expr)
         end
     end
 end
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 module TestModule1
 
@@ -41,15 +38,34 @@ function h end
 
 end # module TestModule1
 
-@test Base.isexported(TestModule1, :f)
-@test !Base.isexported(TestModule1, :g)
-@test !Base.isexported(TestModule1, :h)
+function _public_names(mod::Module)
+    result = Base.names(
+        mod;
+        all = false,
+        imported = false,
+    )
+    return result
+end
 
-# @test Base.ispublic(TestModule1, :f)
-# @test !Base.ispublic(TestModule1, :h)
+@testset "Tests for TestModule1" begin
+    @test Base.isexported(TestModule1, :f)
+    @test !Base.isexported(TestModule1, :g)
+    @test !Base.isexported(TestModule1, :h)
 
-@static if Base.VERSION >= v"1.11.0-DEV.469"
-    # @test Base.ispublic(TestModule1, :g)
-else
-    # @test !Base.ispublic(TestModule1, :g)
+    # `Base.ispublic` was added in Julia 1.11
+    @static if Base.VERSION >= v"1.11.0-DEV.469"
+        @test Base.ispublic(TestModule1, :f)
+        @test Base.ispublic(TestModule1, :g)
+        @test !Base.ispublic(TestModule1, :h)
+    end
+
+    @static if Base.VERSION >= v"1.11.0-DEV.469"
+        @test :f ∈ _public_names(TestModule1)
+        @test :g ∈ _public_names(TestModule1)
+        @test :h ∉ _public_names(TestModule1)
+    else
+        @test :f ∈ _public_names(TestModule1)
+        @test :g ∉ _public_names(TestModule1)
+        @test :h ∉ _public_names(TestModule1)
+    end
 end
